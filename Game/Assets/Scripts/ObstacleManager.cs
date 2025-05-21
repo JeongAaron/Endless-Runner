7ecba64bc896733681cc7a9a4c46f85c75e19cab
JeongAaron <lovejsk1004@gmail.com>
2025-05-21 17:54:52 +0900

    2025-05-20 EndlessRunner Day 6

--- Game/Assets/Scripts/ObstacleManager.cs
+++ Game/Assets/Scripts/ObstacleManager.cs
@@ -1,19 +1,61 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class ObstacleManager : MonoBehaviour
 {
     [SerializeField] List<GameObject> obstacles;
-    [SerializeField] List<GameObject> random;
+    [SerializeField] GameObject [ ] prefab;
+    [SerializeField] int createCount = 5;
+    [SerializeField] Transform [ ] transforms;
+    [SerializeField] float activeTime = 5;
+    [SerializeField] int random;
     void Start()
     {
-      
+        Create();
+        StartCoroutine(ActiveObstacle());
     }
-
-    // Update is called once per frame
-    void Update()
+    public void Create()
     {
-        
+        for(int i = 0; i < createCount; i++)
+        {
+            GameObject clone = Instantiate(prefab[Random.Range(0,prefab.Length)],transform);
+            clone.SetActive(false);
+            obstacles.Add(clone);
+            
+        }
+    }
+    public IEnumerator ActiveObstacle()
+    {
+        while(true)
+        { 
+            random = Random.Range(0,obstacles.Count);
+            while (obstacles[random].activeSelf == true)
+            {
+                random = (random + 1) % obstacles.Count;
+            }
+            obstacles[random].transform.position = transforms[Random.Range(0, transforms.Length)].position;
+            obstacles[random].SetActive(true);
+            if(ExamineActive()  == true)
+            {
+                GameObject clone = Instantiate(prefab[Random.Range(0,prefab.Length)],transform);
+                clone.SetActive(false);
+                obstacles.Add(clone);
+            }
+            yield return new WaitForSeconds(activeTime);
+        }
+    }
+    bool ExamineActive()
+    {
+        for (int i = 0; i < obstacles.Count; i++)
+        {
+            if (obstacles[i].activeSelf != true)
+            {
+                return false;
+            }
+        }
+        return true;
     }
 }
